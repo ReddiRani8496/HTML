8f94d4f961fa924f8403dc047a6912f53764f07c
reddirani8496 <reddiranibattuluri@gmail.com>
2023-09-13 23:48:26 +0530

    Day-18 programs done

--- Day-18/ToDoList/ToDoList.js
+++ Day-18/ToDoList/ToDoList.js
@@ -1,8 +1,17 @@
-function add() {
-    let input = document.getElementsByTagName('input')[0];
-    let ol = document.getElementsByTagName('ol')[0];
-    let li = document.createElement("li");
-    li.innerText=input.value;
-    ol.appendChild(li);
-    input.value="";
+let input = document.querySelector('input');
+let items=[];
+function addItem() {
+    let value = input.value;
+    items.unshift(value);
+    dispalyItems();
+}
+function dispalyItems() {
+    let ol = document.querySelector("ol");
+    ol.innerText="";
+    for(let i=0;i<items.length;i++) {
+        let li = document.createElement('li');
+        let node = document.createTextNode(items[i]);
+        li.appendChild(node);
+        ol.appendChild(li);
+    }
 }
