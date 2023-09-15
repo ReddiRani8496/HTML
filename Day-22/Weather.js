5ccb0fedbdf02c2aa2839d072d8396f44af4c115
reddirani8496 <reddiranibattuluri@gmail.com>
2023-09-15 17:56:08 +0530

    Day-22 programs done

--- Day-22/Weather.js
+++ Day-22/Weather.js
@@ -7,7 +7,7 @@ btn.addEventListener('click',getWeather);
 async function getWeather() {
     let city = document.querySelector('input');
     cityName = city.value;
-    let url1 ="https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=034576bb06cc892c279eb2eec83e5ead";
+    let url1 =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=034576bb06cc892c279eb2eec83e5ead`;
     let response = await fetch(url1);
     let weather = await response.json();
     console.log(weather);
