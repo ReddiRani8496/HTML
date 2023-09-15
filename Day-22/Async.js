1e57ffc41119be8ac9d008f60fd5468ca0ad8283
reddirani8496 <reddiranibattuluri@gmail.com>
2023-09-15 17:55:04 +0530

    Day-22 programs done

---
+++ Day-22/Async.js
@@ -0,0 +1,12 @@
+function hi() {
+    console.log('hi');
+}
+function hello(){
+    console.log('hello');
+}
+function sayhi(){
+    console.log('say hi');
+}
+hi();
+setInterval(hello,2000);
+sayhi();
---
+++ Day-22/Weather.js
@@ -0,0 +1,14 @@
+let url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=034576bb06cc892c279eb2eec83e5ead";
+
+let cityName = 'hyderabad';
+let btn = document.querySelector('button');
+btn.addEventListener('click',getWeather);
+
+async function getWeather() {
+    let city = document.querySelector('input');
+    cityName = city.value;
+    let url1 ="https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=034576bb06cc892c279eb2eec83e5ead";
+    let response = await fetch(url1);
+    let weather = await response.json();
+    console.log(weather);
+}
