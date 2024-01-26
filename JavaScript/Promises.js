0efac4fe2a56eb1adf6e0a0923ce0666466ef128
reddirani8496 <reddiranibattuluri@gmail.com>
2024-01-26 19:54:13 +0530

    promises added

---
+++ JavaScript/Promises.js
@@ -0,0 +1,48 @@
+const cart = ["shoes", "pant", "kurta"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    // create order
+    // validate cart
+    // validate id
+
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+
+    const orderId = "12345";
+    if (orderId) {
+      setTimeout(function () {
+        resolve(orderId);
+      }, 5000);
+    }
+  });
+
+  return pr;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment successful");
+  });
+}
+
+function validateCart(cart) {
+  return true;
+}
