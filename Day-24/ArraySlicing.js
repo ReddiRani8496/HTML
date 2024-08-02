1cb7a32adb588a169923f2a902503a1eee7e071a
reddirani8496 <reddiranibattuluri@gmail.com>
2024-08-02 22:36:38 +0530

    array splicing and slicing programs uploaded

---
+++ Day-24/ArraySlicing.js
@@ -0,0 +1,6 @@
+// splicing means it will form a new array by taking starting and ending index
+const arr = [10, 20, 30, 40, 50, 60, 70, 80];
+
+console.log(arr.slice(2, 4));
+
+console.log(arr);
---
+++ Day-24/ArraySplicing.js
@@ -0,0 +1,6 @@
+const arr = [10, 20, 30, 40, 50, 60, 70, 80];
+
+// splicing means it will remove elements from starting index, next value represents number of elements
+console.log(arr.splice(2, 3));
+
+console.log(arr);
