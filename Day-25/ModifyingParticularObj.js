9d19ef50bf0c118125504b7d649ae9ed648f7e96
reddirani8496 <reddiranibattuluri@gmail.com>
2024-08-03 20:11:41 +0530

    Modifying particular object

---
+++ Day-25/ModifyingParticularObj.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    id: "1",
+    name: "john",
+    age: 25,
+  },
+  {
+    id: "2",
+    name: "jane",
+    age: 30,
+  },
+  {
+    id: "3",
+    name: "mike",
+    age: 28,
+  },
+  {
+    id: "4",
+    name: "sarah",
+    age: 22,
+  },
+];
+
+const changeUserName = (name) => {
+  const newUsersArray = users.filter((user) => user.name === name);
+  return newUsersArray;
+};
+
+console.log(changeUserName("jane")); // [ { id: '2', name: 'jane', age: 30 } ]
