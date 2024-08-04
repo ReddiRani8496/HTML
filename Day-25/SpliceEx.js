5dd89a8e4950444d25b47062c8db0cec6f7a53c5
reddirani8496 <reddiranibattuluri@gmail.com>
2024-08-04 15:06:36 +0530

    splice program uploaded

---
+++ Day-25/SpliceEx.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: 1,
+    name: "rahul",
+    age: 25,
+  },
+  {
+    id: 2,
+    name: "rohit",
+    age: 30,
+  },
+  {
+    id: 3,
+    name: "rakesh",
+    age: 28,
+  },
+  {
+    id: 4,
+    name: "rajesh",
+    age: 22,
+  },
+];
+
+users.splice(2, 1);
+console.log(users);
